c04c6dea7f44dadbd4b18f4901683b0b792566e7
Afanaseva97 <35865283+Afanaseva97@users.noreply.github.com>
2020-02-17 11:58:43 +0300

    Update Comparing_of_products.py

--- features/step/Comparing_of_products.py
+++ features/step/Comparing_of_products.py
@@ -2,18 +2,19 @@
 
 from behave import *
 
-@given('I am finding a tile with "{number1}" less than 1000 rub/m2, the "{number2}" is higher then 6, the "{number3}" is higher then 6')
+@given('I am finding a tile with "{price}" less than 1000 rub/m2, the "{strengh}" is higher then 6, the "{usage}" is higher then 6')
    def step_impl(context):
-   pass
+   assert context.table is not None
 
 @when('When I launch a search for all stores')
-def step_impl(context):
-    context.browser.get('??')
-    form = get_element(context.browser, tag='form')
-    get_element(form, name="msisdn").send_keys('??')
-    form.submit()
+def step_impl(context,name,price,strengh,usage):
+for price<1000 and strengh>6 and c>6:
+   assert_that(context.name, equal_to(product))
+   assert_that(context.number1, equal_to(price))
+   assert_that(context.number2, equal_to(strengh))
+   assert_that(context.number3, equal_to(usage))
+ 
 
 @then('Then there are 2 variants of the tile that meet the requirements with the articles 1001 and 1002')
-def step_impl(context, number1, number2, number3):
-    if number1 < "1000" and number2 > "6" and number3 > "6":
-    assert_that (
+def step_impl(context,name, number1, number2, number3)
+    print("Product:%s, Price: %s, Streght: %s, Usage: %s" % (name, number1,number2, number3))
